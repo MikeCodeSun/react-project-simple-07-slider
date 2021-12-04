e7227fe4b9c0924ae145045936d8490721086247
mike <mike@mikedeiMac.local>
2021-12-04 18:34:21 +0800

    feature: simple slide react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import data from "./data";
 
 function App() {
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    if (index < 0) {
+      setIndex(people.length - 1);
+    }
+    if (index > people.length - 1) {
+      setIndex(0);
+    }
+  }, [index, people]);
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index - 1);
+    }, 3000);
+    return () => {
+      clearInterval(slider);
+    };
+  }, [index]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>Reviews
+        </h2>
+        <div className="title-underline"></div>
+      </div>
+      <div className="container">
+        {people.map((person, personIndex) => {
+          const { id, name, image, title, quote } = person;
+          let position = "nextSlide";
+          if (index === personIndex) {
+            position = "activeSlide";
+          }
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+          ) {
+            position = "lastSlide";
+          }
+          return (
+            <article className={position} key={id}>
+              <img src={image} alt={name} className="img person-img" />
+              <div className="info">
+                <h4>{name}</h4>
+                <h4>{title}</h4>
+                <p>{quote}</p>
+              </div>
+            </article>
+          );
+        })}
+        <button
+          className="btn btn-hipster btn-prev"
+          onClick={() => setIndex(index - 1)}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          &#8592;
+        </button>
+        <button
+          className="btn btn-hipster btn-next"
+          onClick={() => setIndex(index + 1)}
+        >
+          &#8594;
+        </button>
+      </div>
+    </section>
   );
 }
 
